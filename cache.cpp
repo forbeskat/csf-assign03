8fe0c18e5989e6ef4e1da179075ff44b88b21e52
Annie Wang <awang105@jh.edu>
2023-10-09 18:38:10 -0400

    here

---
+++ cache.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    
+    int total_loads;
+    int total_stores = 0;
+    int load_hits = 0;
+    int load_misses = 0;
+    int store_hits = 0;
+    int store_misses = 0;
+    int total_cycles = 0;
+
+
+    string line;
+
+    while(getline(cin, line)) {
+        istringstream iss(line);
+        string field1;
+        string address;
+        string someotherstuff; //come back to this thing in part 2
+        
+        //read in a file with the format
+        // (l or s) (address) (some other stuff)
+        iss >>field1>>address>>someotherstuff ;
+
+        if (field1 == "l"){ //loading
+            //
+        } else if (field1 == "s"){ //storing
+            //
+        } else {
+            cout << "error: invalid input" << endl;
+        }
+    }
+
+    return 0;
+}
