15f51d026658096cdd9779ed20027c906f1d6337
Annie Wang <awang105@jh.edu>
2023-10-22 15:04:21 -0400

    here

--- main.cpp
+++ main.cpp
@@ -6,12 +6,33 @@
 
 using namespace std;
 
-int check_in(int argc){
-    //check if the input is valid
-    //if it is, return 1
-    //if it isn't, return 0
-    return true;
+//checks if n is a power of 2
+int check_2(int n){
+    return (n!= 0) && ((n & (n-1)) == 0);
 }
+
+int check_in(int argc, char *argv[]){
+    int ret = 0;
+    if (argc != 7){
+        cerr << "error: invalid number of arguments" << endl;
+        ret = 1;
+    }
+    if (check_2(atoi(argv[1])) == 0){                 //should it be atoi or stoi check before submitting
+        cerr << "error: invalid number of sets" << endl;
+        ret = 0;
+    }
+    if (check_2(atoi(argv[2])) == 0){
+        cerr << "error: invalid number of blocks" << endl;
+        ret = 0;
+    }
+    if (check_2(atoi(argv[3])) == 0){
+        cerr << "error: invalid number of words" << endl;
+        ret = 0;
+    }
+    return ret;
+}
+
+
 void store(string address, vector<string> &cache, int &total_stores, int &store_hits, int &store_misses){
     //store the address in the cache
     //if it's already there, increment store_hits
@@ -19,13 +40,11 @@ void store(string address, vector<string> &cache, int &total_stores, int &store_
     //increment total_stores
 }
 
-
 int main(int argc, char *argv[]){
 
 
     //check if command line input is valid
     if (!check_in(argc)){
-        cerr << "error: invalid input" << endl;
         return 1;
     }
 
@@ -33,7 +52,7 @@ int main(int argc, char *argv[]){
 
 
     
-    int total_loads;
+    int total_loads = 0;
     int total_stores = 0;
     int load_hits = 0;
     int load_misses = 0;
@@ -46,7 +65,7 @@ int main(int argc, char *argv[]){
 
     while(getline(cin, line)) {
         istringstream iss(line);
-        string l_or_s;
+        string ls;
         string address;
         string someotherstuff; //come back to this thing in part 2
         
@@ -54,7 +73,7 @@ int main(int argc, char *argv[]){
         // (l or s) (address) (some other stuff)
         iss >>field1>>address>>someotherstuff ;
 
-        if (field1 == "l"){ //loading
+        if (ls == "l"){ //loading
             load();
         } else if (field1 == "s"){ //storing
             store();
