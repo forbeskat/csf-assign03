9ce3ad3b86a338b2ede31dd53a1aa08bb2cefd7c
Katherine Forbes <kforbes6@jh.edu>
2023-10-25 16:05:58 -0400

    changes

--- csim_funcs.h
+++ csim_funcs.h
@@ -13,6 +13,15 @@ bool not_power_of_two(int num);
 /* Initialize the cache according to the configurations given by user */
 Cache init_cache(char **argv);
 
+int loadHit(Cache* cache, unsigned int index, unsigned int tag);
+
+void writeThrough(int* totalCycles);
+
+void writeBack(Cache* cache, unsigned int index, unsigned int tag);
+
+int loadMiss(Cache* cache, unsigned int index, unsigned int tag);
+
+void noWriteAllocate(int* totalCycles);
 
 
 #endif
