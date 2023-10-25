d1888cffb343da8f58cd9c0436a2167a4bb54274
Katherine Forbes <kforbes6@jh.edu>
2023-10-25 12:41:04 -0400

    cache configs

---
+++ no_allocate_through.h
@@ -0,0 +1,13 @@
+/* Configuration for no-write-allocate and write-through */
+
+#ifndef NO_ALLOCATE_THROUGH_CACHE
+#define NO_ALLOCATE_THROUGH_CACHE
+
+#include "cache.h"
+
+class NO_ALLOCATE_THROUGH_cache : public Cache {
+
+    
+}
+
+#endif
